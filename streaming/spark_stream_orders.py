c92d92fb68271f1e1064a516f92c8d2a1ebcf586
Alejandro <yalejandrocamposr@gmail.com>
2026-03-12 18:42:43 -0500

    Implementación de escritura en BQ con intentos

--- streaming/spark_stream_orders.py
+++ streaming/spark_stream_orders.py
@@ -42,8 +42,8 @@ subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 
-# Buffer de eventos
-messages = [] 
+
+messages = [] # Buffer de eventos
 
 # Callback que se ejecuta cada vez que llega un evento desde Pub/Sub
 def callback(message):
@@ -53,8 +53,7 @@ def callback(message):
     logger.info("Received new event from Pub/Sub")
     message.ack()  
 
-# Cada vez que llegue un mensaje ejecutar callback
-subscriber.subscribe(subscription_path, callback=callback) 
+subscriber.subscribe(subscription_path, callback=callback) # Cada vez que llegue un mensaje ejecutar callback
 
 
 # Calidad de datos
@@ -65,6 +64,31 @@ def validate_orders(df):
     df = df.dropDuplicates(["order_id"])
     return df
 
+def write_with_retry(df, table_name, max_retries=3): 
+    attempt = 1
+    while attempt <= max_retries:
+        try:
+            logger.info(f"Writing to {table_name} (attempt {attempt})")
+
+            df.write \
+                .format("bigquery") \
+                .option("table", table_name) \
+                .option("temporaryGcsBucket", "meli-data-platform-temp") \
+                .mode("append") \
+                .save()
+            
+            logger.info(f"Write successful to {table_name}")
+            return
+
+        except Exception as e:
+
+            logger.error(f"Write failed on attempt {attempt}: {str(e)}")
+            attempt += 1
+            time.sleep(5)
+
+    logger.error(f"All retries failed for table {table_name}")
+
+
 
 logger.info("Listening for messages...")
 
@@ -74,8 +98,8 @@ def process_batch(batch_messages):
 
     logger.info(f"Processing batch with {len(batch_messages)} records")
 
-    # Convertir lista de eventos en DataFrame de Spark
-    df = spark.createDataFrame(batch_messages)
+
+    df = spark.createDataFrame(batch_messages) # Convertir lista de eventos en DataFrame de Spark
     
     original_count = df.count() # Conteo de registros crudos
     
@@ -91,41 +115,25 @@ def process_batch(batch_messages):
     logger.info(f"Batch processing time: {processing_time} seconds")
 
     # Calcular valor total de cada orden
-    df = df.withColumn(
+    df_raw = df.withColumn(
         "total_value",
         col("price") * col("quantity")
     )
 
-    logger.info("Writing RAW events table orders_stream")
-    # Guardar RAW events
-    df.write \
-        .format("bigquery") \
-        .option("table", "meli-data-platform.meli_raw.orders_stream") \
-        .option("temporaryGcsBucket", "meli-data-platform-temp") \
-        .mode("append") \
-        .save()
+    write_with_retry(df_raw, "meli-data-platform.meli_raw.orders_stream") # Guardar RAW events
     
-    ## Transformación de datos
+    #### Transformación de datos ####
 
     # Agregación: ventas totales por categoría
-    sales_by_category = df.groupBy("category").agg(
+    df_sales_by_category = df_raw.groupBy("category").agg(
     sum("total_value").alias("total_sales")
 )
     # Agregar timestamp del momento en que se procesa el batch
-    sales_by_category = sales_by_category.withColumn(
+    df_sales_by_category = df_sales_by_category.withColumn(
         "processed_at",
         from_utc_timestamp(current_timestamp(), "America/Bogota")
 )
-
-    logger.info("Writing analytics table sales_by_category")
-    # Guardar analitycs
-    sales_by_category.write \
-        .format("bigquery") \
-        .option("table", "meli-data-platform.meli_analytics.sales_by_category") \
-        .option("temporaryGcsBucket", "meli-data-platform-temp") \
-        .mode("append") \
-        .save()
-
+    write_with_retry(df_sales_by_category, "meli-data-platform.meli_analytics.sales_by_category") # Guardar analitycs
 
 # Loop continuo que procesa eventos cada cierto intervalo (micro-batch)
 while True:
