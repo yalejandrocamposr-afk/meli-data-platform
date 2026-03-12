ba3763c59e1c34be2428d08af6cbd1c5075f0c70
Alejandro <yalejandrocamposr@gmail.com>
2026-03-12 11:02:04 -0500

    Instalación airflow operators

--- airflow/dags/sales_pipeline_dag.py
+++ airflow/dags/sales_pipeline_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.bash import BashOperator # type: ignore
 from datetime import datetime
 
 default_args = {
