2acd8e38c55ff2c6a7de6d18e41ac5cc62fcd785
Alejandro <yalejandrocamposr@gmail.com>
2026-03-12 12:56:57 -0500

    Inclusión de ejecución desde AF de la carpeta models

--- airflow/dags/sales_pipeline_dag.py
+++ airflow/dags/sales_pipeline_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator # type: ignore
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator # type: ignore
 from datetime import datetime
 
 default_args = {
@@ -14,7 +14,17 @@ with DAG(
 ) as dag:
 
 # Cada hora que corre genera en BQ una analtica de categorias vendidas. 
-    run_sales_analytics = BashOperator(
+    with open("/opt/airflow/models/sales_by_category_hourly.sql") as f:
+        sales_query = f.read()
+
+    run_sales_analytics = BigQueryInsertJobOperator(
         task_id="run_sales_analytics",
-        bash_command="bq query --use_legacy_sql=false < /opt/airflow/models/sales_by_category_hourly.sql"
+         project_id="meli-data-platform",
+        configuration={
+            "query": {
+                "query": sales_query,
+                "useLegacySql": False,
+            }
+        },
+        location="US"
     )
