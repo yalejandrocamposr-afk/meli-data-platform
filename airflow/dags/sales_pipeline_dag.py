2f95ddb1d24dac44030f584260c2846a13f86656
Alejandro <yalejandrocamposr@gmail.com>
2026-03-18 10:27:30 -0500

    Exclusión de BQ en DAG

--- airflow/dags/sales_pipeline_dag.py
+++ airflow/dags/sales_pipeline_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator # type: ignore
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator # type: ignore
 from datetime import datetime
 
 default_args = {
@@ -13,18 +13,22 @@ with DAG(
     default_args=default_args
 ) as dag:
 
-# Cada que corre genera en BQ una analtica de categorias vendidas del día. 
-    run_analytics_model = BashOperator(
+    run_analytics_model = BigQueryInsertJobOperator(
         task_id="run_sales_analytics",
-        bash_command="""
-        bq query --use_legacy_sql=false '
-        CREATE OR REPLACE TABLE `meli-data-platform.meli_analytics.sales_by_category_hourly` AS
-        SELECT
-            category,
-            SUM(total_value) as total_sales
-        FROM `meli-data-platform.meli_raw.orders`
-        WHERE DATE(event_time) = CURRENT_DATE()
-        GROUP BY category
-        '
-        """
+        project_id="meli-data-platform",
+        configuration={
+            "query": {
+                "query": """
+                CREATE OR REPLACE TABLE `meli-data-platform.meli_analytics.sales_by_category_hourly` AS
+                SELECT
+                    category,
+                    SUM(total_value) AS total_sales
+                FROM `meli-data-platform.meli_raw.orders_streams`
+                WHERE DATE(event_time) = CURRENT_DATE()
+                GROUP BY category
+                """,
+                "useLegacySql": False,
+            }
+        },
+        location="US",
     )
